5aa76012c2dd50aa11d0a69ac2f29ac3cd178858
Tri Nanda <2001020024@student.umrah.ac.id>
2025-03-03 17:07:27 +0700

    Added: Pengguna resource filament

---
+++ app/Filament/Resources/PenggunaResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PenggunaResource\Pages;
+use App\Filament\Resources\PenggunaResource\RelationManagers;
+use App\Models\Pengguna;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PenggunaResource extends Resource
+{
+    protected static ?string $model = Pengguna::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama')
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('image')
+                    ->image(),
+                Forms\Components\TextInput::make('nomor_hp')
+                    ->maxLength(15)
+                    ->default(null),
+                Forms\Components\TextInput::make('nama_toko')
+                    ->maxLength(100)
+                    ->default(null),
+                Forms\Components\DateTimePicker::make('tanggal_bergabung')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('terakhir_login'),
+                Forms\Components\Toggle::make('is_active')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('nomor_hp')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nama_toko')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('tanggal_bergabung')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('terakhir_login')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPenggunas::route('/'),
+            'create' => Pages\CreatePengguna::route('/create'),
+            'edit' => Pages\EditPengguna::route('/{record}/edit'),
+        ];
+    }
+}
